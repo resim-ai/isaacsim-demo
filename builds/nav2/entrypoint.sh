ce06d13e2e2602e86628d34cecd9b5acd95ada4f
Michael Bauer <michael@resim.ai>
2025-06-25 20:13:56 +0000

    um

--- builds/nav2/entrypoint.sh
+++ builds/nav2/entrypoint.sh
@@ -3,8 +3,10 @@ set -e
 
 source /opt/ros/humble/setup.bash
 source /humble_ws/install/setup.bash
-ros2 topic pub /planner_selector std_msgs/String "data: 'SmacPlannerHybrid'" --once
+
+ros2 topic pub /planner_selector std_msgs/String "data: 'SmacPlannerHybrid'" --once &
 ros2 launch carter_navigation carter_navigation_isaacsim.launch.py
+
 
 mv /tmp/resim/outputs/record/* /tmp/resim/outputs 
 rm -rf /tmp/resim/outputs/record
