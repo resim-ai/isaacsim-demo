54bc6142195e6cc5682a728f261fb70130091a59
Michael Bauer <michael@resim.ai>
2025-08-29 22:22:43 +0000

    Working

--- humble_ws/src/checklist/checklist/checklist.py
+++ humble_ws/src/checklist/checklist/checklist.py
@@ -7,13 +7,10 @@ from rclpy.task import Future
 
 class Checklist(Node):
     def __init__(self, future):
-        super().__init__('checklist')
+        super().__init__("checklist")
         self.future = future
         self.subscription = self.create_subscription(
-            TFMessage,
-            '/tf',
-            self.listener_callback,
-            10
+            TFMessage, "/tf", self.listener_callback, 10
         )
         self.received_odom = False
 
@@ -25,12 +22,12 @@ class Checklist(Node):
 
     def listener_callback(self, msg: TFMessage):
         for t in msg.transforms:
-            if t.header.frame_id == 'odom' and t.child_frame_id == 'base_link':
+            if t.header.frame_id == "odom" and t.child_frame_id == "base_link":
                 self.received_odom = True
-                self.get_logger().info('Received base_link -> odom')
+                self.get_logger().info("Received base_link -> odom")
 
         if self.received_odom:
-            self.get_logger().info('Isaac is publishing on /tf... Starting Nav2')
+            self.get_logger().info("Isaac is publishing on /tf... Starting Nav2")
             self.future.set_result(True)
 
 
@@ -43,5 +40,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
