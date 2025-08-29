818e1e667be37083b49e59a4f60155e5428c6e46
Matt C <matt.coomber@bluerivertech.com>
2025-08-29 20:52:15 +0000

    Update images

--- scripts/rebuild_and_push_cache.sh
+++ scripts/rebuild_and_push_cache.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-if [ ! -f "build/isaac-cache.tar.gz" ]; then
+if [ ! -f "build/shadercache.tar.gz" ]; then
     mkdir -p build
-    aws s3 cp s3://resim-binaries/isaac-cache/isaac-cache-575-4-5-0.tar.gz build/isaac-cache.tar.gz
+    aws s3 cp s3://resim-binaries/isaac-cache/shadercache-575-5-0-0.tar.gz build/shadercache.tar.gz
 fi
 
-docker build . -f builds/isaacsim/isaacsim.cache.dockerfile -t 909785973729.dkr.ecr.us-east-1.amazonaws.com/isaacsim-test-images:isaacsim-mcb-isaacsim-shader-cache
-docker push 909785973729.dkr.ecr.us-east-1.amazonaws.com/isaacsim-test-images:isaacsim-mcb-isaacsim-shader-cache
+docker build . -f builds/isaacsim/isaacsim.cache.dockerfile -t 909785973729.dkr.ecr.us-east-1.amazonaws.com/isaacsim-test-images:isaacsim-mcb-isaacsim-5-0-0-shader-cache
+docker push 909785973729.dkr.ecr.us-east-1.amazonaws.com/isaacsim-test-images:isaacsim-mcb-isaacsim-5-0-0-shader-cache
