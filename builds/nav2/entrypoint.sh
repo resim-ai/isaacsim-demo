77ade90f12629f1c78f0434127e775ad97a52e6b
Pete van Wesep <pete@resim.ai>
2026-03-13 13:36:15 -0700

    try again

--- builds/nav2/entrypoint.sh
+++ builds/nav2/entrypoint.sh
@@ -26,7 +26,7 @@ while [ ! -f /tmp/isaac_ready ]; do
     sleep 1
 done
 
-TIMEOUT="${RESIM_NAV2_TIMEOUT:-30}"
+TIMEOUT="${RESIM_NAV2_TIMEOUT:-420}"
 SECONDS=0
 while [ $SECONDS -lt $TIMEOUT ]; do
     if ! kill -0 $LAUNCH_PID 2>/dev/null; then
